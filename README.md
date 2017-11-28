557cef5114c3067e90ecefb2b7031f8491983e5a
emptyport <michaelporter.chemistry@gmail.com>
2017-11-28 17:09:02 -0600

    started working on the actual parsing. some useful stuff in example.js

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # js_mzml
 
 spectra in test folder from http://psidev.info/index.php?q=node/257
+
+https://github.com/cheminfo-js/mzML helped figure out how to decode the m/z and intensity arrays
+useful info here: http://www.bryanesmith.com/docs/reading-binary-data-mzml/
+
--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+var base64 = require('base64-js');
+var pako = require('pako');
+
 module.exports = class JsMzml {
-  constructor() {
+  constructor(filename) {
+    this.filename = filename;
+  }
+
+  retrieve(options) {
+    var options = options || {};
+    var level = options.level || "Both";
+    var RT_begin = options.RT_begin || 0;
+    var RT_end = options.RT_end || 9999999999;
+
+
+
+
+    return {};
   }
   
   hello() {
