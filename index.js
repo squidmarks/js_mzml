03c6f2dc7a980df89a11d219bef3bc3f10466d87
emptyport <michaelporter.chemistry@gmail.com>
2017-11-29 16:16:52 -0600

    got stuff working. it might not be the best way to do things, but it works

--- index.js
+++ index.js
@@ -1,21 +1,125 @@
 var base64 = require('base64-js');
 var pako = require('pako');
+var saxStream = require('sax').createStream(true);
+var fs = require('fs');
 
 module.exports = class JsMzml {
   constructor(filename) {
     this.filename = filename;
+    this.spectra = {};
+    this.isFinished = true;
   }
 
+  // The following code is heavily based on https://github.com/cheminfo-js/mzML
+  // Changes have been made to suit my use case
   retrieve(options) {
     var options = options || {};
     var level = options.level || "Both";
-    var RT_begin = options.RT_begin || 0;
-    var RT_end = options.RT_end || 9999999999;
+    var rtBegin = options.rtBegin || 0;
+    var rtEnd = options.rtEnd || 9999999999;
 
+    this.isFinished = false;
+    var entry = {};
+    var currentId;
+    var kind;
+    var nextValue;
+    var readRaw;
+    var bitType;
+    var isCompressed;
 
+    var self = this;
 
+    saxStream.on("opentag", function(node) {
+      readRaw = node.name === 'binary';
 
-    return {};
+      switch (node.name) {
+        case 'mzML':
+          kind = 'mzML';
+          break;
+        case 'spectrum':
+          if (node.attributes.id) {
+            if (Object.keys(entry).length > 0) {
+              if (entry.msLevel === level || level === 'Both') {
+                if (entry.time <= rtEnd && entry.time >= rtBegin) {
+                  self.spectra[entry.currentId] = Object.assign({}, entry);
+                  entry = {};                  
+                }
+              }
+            }
+            currentId = node.attributes.id;
+            entry.currentId = currentId;
+          }
+          break;
+        case 'chromatogram':
+          currentId = undefined;
+          break;
+        case 'cvParam':
+          switch (node.attributes.name) {
+              case 'm/z array':
+                nextValue = 'MASS';
+                break;
+              case 'intensity array':
+                nextValue = 'INTENSITY';
+                break;
+              case 'scan start time':
+                entry.time = parseFloat(node.attributes.value);
+                break;
+              case '64-bit float':
+                bitType = '64';
+                break;
+              case '32-bit float':
+                bitType = '32';
+                break;
+              case 'zlib compression':
+                isCompressed = true;
+                break;
+              case 'ms level':
+                entry.msLevel = node.attributes.value;
+                break;
+              default:
+                nextValue = null;
+                break;
+          }
+          break;
+      }
+    });
+
+    saxStream.on("text", function(raw) {
+      if (readRaw && currentId) {
+        if (nextValue === 'MASS') {
+          entry.mass = self.decodeData(raw, bitType, isCompressed);
+        } else if (nextValue === 'INTENSITY') {
+          entry.intensity = self.decodeData(raw, bitType, isCompressed);
+        }
+        nextValue = null;
+      }
+    });
+
+    saxStream.on("end", function() {
+      self.isFinished = true;
+    });
+
+    fs.createReadStream(this.filename)
+      .pipe(saxStream);
+  }
+
+  decodeData(raw, bitType, isCompressed) {
+    var array = [];
+    var buffer = base64.toByteArray(raw);
+    if (isCompressed) {
+      buffer = pako.inflate(buffer);
+    }
+
+    if (bitType === '32') {
+      return new Float32Array(buffer.buffer);
+    }
+    else if (bitType === '64') {
+      return new Float64Array(buffer.buffer);
+    }
+    else {
+      return [];
+    }
+    return [];
   }
   
   hello() {
